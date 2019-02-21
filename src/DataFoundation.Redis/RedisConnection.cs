b74fd1d36c81d77fe7e29233dfcdf2fece7f93f6
Jeferson Tenorio <jefersonsv@gmail.com>
2019-02-21 15:26:03 -0300

    initial commit

---
+++ src/DataFoundation.Redis/RedisConnection.cs
@@ -0,0 +1,20 @@
+﻿using StackExchange.Redis;
+
+namespace DataFoundation.Redis
+{
+    public class RedisConnection
+    {
+        private ConnectionMultiplexer redis;
+
+        public RedisConnection(string redisConnectionString)
+        {
+            if (string.IsNullOrEmpty(redisConnectionString))
+                redisConnectionString = "127.0.0.1:6379";
+
+            redis = ConnectionMultiplexer.Connect(redisConnectionString);
+            DB = redis.GetDatabase();
+        }
+
+        public IDatabase DB { get; private set; }
+    }
+}
